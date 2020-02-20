6a117627868c7cc55aaf66e0eb5617b40b658af6
Viktor <viktor.b@ramotion.agency>
2020-02-20 13:02:23 +0300

    Fix page scrolling on menu appearing.

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,9 +6,28 @@ document.getElementById('navbarMenuButton').addEventListener('click', function(e
 });
 
 function openMenu() {
+    const body = document.body;
     let navBarMenu = document.getElementById('navbarMenu');
     let menuButtonImage = document.getElementById('menuButtonImage')
     
     navBarMenu.classList.toggle('navbar-menu-shown');
-    menuButtonImage.src = navBarMenu.classList.contains('navbar-menu-shown') ? '/assets/images/icons/CrossIcon.svg' : '/assets/images/icons/MenuIcon.svg';
+
+    if (navBarMenu.classList.contains('navbar-menu-shown')) {
+        const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
+        body.style.position = 'fixed';
+        body.style.top = `-${scrollY}`;
+
+        menuButtonImage.src = '/assets/images/icons/CrossIcon.svg';
+    } else {
+        const scrollY = body.style.top;
+        body.style.position = '';
+        body.style.top = '';
+        window.scrollTo(0, parseInt(scrollY || '0') * -1);
+
+        menuButtonImage.src = '/assets/images/icons/MenuIcon.svg';
+    }
 }
+
+window.addEventListener('scroll', () => {
+    document.documentElement.style.setProperty('--scroll-y', `${window.scrollY}px`);
+});
