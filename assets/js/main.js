9dbf531742c8d31f26f35867581ad51f74e1fcff
Viktor <viktor.b@ramotion.agency>
2020-03-04 13:06:21 +0300

    Fix modal position

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,12 +15,17 @@ window.addEventListener('click', (event) => {
 
 document.getElementById('showModalBtn').addEventListener('click', (event) => {
     event.preventDefault();
-    showModalView();
+    toggleModalVisibility();
+});
+
+document.getElementById('joinUsButton').addEventListener('click', (event) => {
+    event.preventDefault();
+    toggleModalVisibility();
 });
 
 document.getElementById('modalCloseButton').addEventListener('click', (event) => {
     event.preventDefault();
-    showModalView();
+    toggleModalVisibility();
 });
 
 document.getElementById('navbarMenuButton').addEventListener('click', (event) => {
@@ -34,35 +39,33 @@ document.getElementById('toTheTopButton').addEventListener('click', (event) => {
 });
 
 function toggleMenu() {
-    // const body = document.body;
     let navBarMenu = document.getElementById('navbarMenu');
     let menuButtonImage = document.getElementById('menuButtonImage')
 
     if (navBarMenu.classList.contains('navbar-menu-shown')) {
-        // const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
-        // body.style.position = 'fixed';
-        // body.style.top = `-${scrollY}`;
         navBarMenu.classList.remove('navbar-menu-shown');
         menuButtonImage.src = '/assets/images/icons/MenuIcon.svg';
     } else {
-        // const scrollY = body.style.top;
-        // body.style.position = '';
-        // body.style.top = '';
-        // window.scrollTo(0, parseInt(scrollY || '0') * -1);
         navBarMenu.classList.add('navbar-menu-shown');
         menuButtonImage.src = '/assets/images/icons/CrossIcon.svg';
     }
 }
 
-function showModalView() {
+function toggleModalVisibility() {
+    const body = document.body;
     let modalView = document.getElementById('modalWindow');
-    modalView.classList.contains('modal-shown') ? modalView.classList.remove('modal-shown') : modalView.classList.add('modal-shown');
-    // if (modalView.classList.contains('modal-shown')) {
-    //     modalView.classList.remove('modal-shown');
-    // } else {
-
-    // }
-    // modalView.classList.toggle('modal-shown');
+    if (modalView.classList.contains('modal-shown')) {
+        const scrollY = body.style.top;
+        body.style.position = '';
+        body.style.top = '';
+        window.scrollTo(0, parseInt(scrollY || '0') * -1);
+        modalView.classList.remove('modal-shown');
+    } else {
+        const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
+        modalView.classList.add('modal-shown');
+        body.style.position = 'fixed';
+        body.style.top = `-${scrollY}`;
+    }
 }
 
 function scrollToTheTop() {
